5339ae54f08257f0e0ced94423303e3f6d6667ae
ab-94 <62454912+ab-94@users.noreply.github.com>
2020-08-05 21:41:39 +1000

    Test

---
+++ test/New folder/nav_drop.js
@@ -0,0 +1,22 @@
+function drop_down() {
+	document.getElementById("dropdown-ctgry-food").classList.toggle("show");
+}
+	
+window.onclick = function(event) 
+{
+	if(!event.target.matches('.dropbtn')) 
+	{
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+			
+		for (i = 0; i < dropdowns.length; i++) 
+		{
+			var openDropdown = dropdowns[i];
+				
+			if(openDropdown.classList.contains('show')) 
+			{
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
